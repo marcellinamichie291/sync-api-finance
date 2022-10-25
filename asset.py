8c868f7ea3657631efce02914898187c40c9bc27
Parzival Yuenyang <krumii.it@gmail.com>
2022-10-25 15:50:55 +0700

    backup db

--- asset.py
+++ asset.py
@@ -31,6 +31,40 @@ def symbols(token):
 
     return None
 
+def get_bit_kub_last_price(token):
+    url = "https://api.bitkub.com/api/market/ticker"
+    response = requests.request("GET", url)
+    data = response.json()
+    for i in data:
+        obj = data[i]
+        exchange = "Bitkub"
+        symbol = str(i)[len("THB_"):]
+        last = obj["last"]
+        lowestAsk = obj["lowestAsk"]
+        highestBid = obj["highestBid"]
+        percentChange = obj["percentChange"]
+        baseVolume = obj["baseVolume"]
+        quoteVolume = obj["quoteVolume"]
+        isFrozen = obj["isFrozen"]
+        high24hr = obj["high24hr"]
+        low24hr = obj["low24hr"]
+        change = obj["change"]
+        prevClose = obj["prevClose"]
+        prevOpen = obj["prevOpen"]
+
+        payload = f'''exchange_id={exchange}&asset_id={symbol}&last_price={last}&lowest_ask={lowestAsk}&highest_bid={highestBid}&percent_change={percentChange}&base_volume={baseVolume}&quote_volume={quoteVolume}&is_frozen={isFrozen}&high_24_hr={high24hr}&low_24_hr={low24hr}&change_total={change}&prev_close={prevClose}&prev_open={prevOpen}&description={i}&is_active=true'''
+        headers = {
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        response = requests.request(
+            "POST", f"{api_host}/asset/lastprice", headers=headers, data=payload)
+
+        print(f"SYNC: {symbol} Status: {response.status_code}")
+
+
+
 
 def logout(token):
     payload={}
@@ -44,5 +78,6 @@ def logout(token):
 
 if __name__ == "__main__":
     token = login()
-    symbols(token)
+    # symbols(token)
+    get_bit_kub_last_price(token)
     logout(token)
